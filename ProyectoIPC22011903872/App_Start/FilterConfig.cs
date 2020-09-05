78d3b585e75d1b36c1b7ad760ee8e7039f10676c
AaronMg2000 <rudy.gopal.2000@gmail.com>
2020-09-05 15:08:07 -0600

    Creación del Proyecto IPC2

---
+++ ProyectoIPC22011903872/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoIPC22011903872
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ ProyectoIPC22011903872/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProyectoIPC22011903872.MvcApplication" Language="C#" %>
