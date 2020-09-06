3904c2233bd7fb1084ed56ed77383530afe207ec
AaronMg2000 <rudy.gopal.2000@gmail.com>
2020-09-05 22:14:09 -0600

    Controlador de usuarios y Base de Datos

---
+++ ProyectoIPC22011903872/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoIPC22011903872.Models;
+using ProyectoIPC22011903872.Models.ViewModels;
+
+namespace ProyectoIPC22011903872.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult getUser()
+        {
+            List<getUserViewModel> ListaUsuarios;
+            using (OthelloEntities db = new OthelloEntities())
+            {
+                ListaUsuarios = (from d in db.USUARIO
+                                 select new getUserViewModel
+                                 {
+                                    Nombre = d.Nombre,
+                                    Apellido = d.Apellido,
+                                    Usuario = d.Usuario1,
+                                    Correo_Electronico = d.Correo_Electronico,
+                                    Codigo_Pais = d.Codigo_Pais,
+                                    Fecha_nacimiento = d.Fecha_nacimiento
+                                 }).ToList();
+            }
+            return View();
+        }
+    }
+}
---
+++ ProyectoIPC22011903872/Models/ViewModels/getUserViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+namespace ProyectoIPC22011903872.Models.ViewModels
+{
+    public class getUserViewModel
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Usuario { get; set; }
+        public string Correo_Electronico { get; set; }
+        public DateTime Fecha_nacimiento { get; set; }
+        public int Codigo_Pais { get; set; }
+
+    }
+}
