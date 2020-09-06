3fa44d737131c1ee006c61d78ee80b2ecc4a1274
AaronMg2000 <rudy.gopal.2000@gmail.com>
2020-09-06 14:38:46 -0600

    Diseño de Login

--- ProyectoIPC22011903872/Controllers/UserController.cs
+++ ProyectoIPC22011903872/Controllers/UserController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -11,6 +12,9 @@ namespace ProyectoIPC22011903872.Controllers
     public class UserController : Controller
     {
         // GET: User
+        public ActionResult Login() {
+            return View();
+        }
         public ActionResult getUser()
         {
             List<getUserViewModel> ListaUsuarios;
@@ -27,6 +31,16 @@ namespace ProyectoIPC22011903872.Controllers
                                     Fecha_nacimiento = d.Fecha_nacimiento
                                  }).ToList();
             }
+            return View(ListaUsuarios);
+        }
+        [HttpGet]
+        public ActionResult Validar(string Usuario, string password) {
+            using (OthelloEntities db = new OthelloEntities()) {
+                db.USUARIO.FirstOrDefault(e => e.Usuario1 == Usuario && e.Contraseña == Encriptar.CrearHASH(password));
+            }
+                return View();
+        }
+        public ActionResult Registrar() {
             return View();
         }
     }
---
+++ ProyectoIPC22011903872/Encriptar.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ProyectoIPC22011903872
+{
+    public class Encriptar
+    {
+        public static string CrearHASH(string str) {
+            SHA256 sha256 = SHA256Managed.Create();
+            ASCIIEncoding encoding = new ASCIIEncoding();
+            byte[] stream = null;
+            StringBuilder sb = new StringBuilder();
+            stream = sha256.ComputeHash(encoding.GetBytes(str));
+            for (int i = 0; i < stream.Length; i++) sb.AppendFormat("{0:x2", stream[i]);
+            return sb.ToString();
+        }
+    }
+}
