f1bcca0c40593a26c7c83f2bb25f5a4fb4688581
AaronMg2000 <rudy.gopal.2000@gmail.com>
2020-09-09 20:37:06 -0600

    Login terminado, Diseño principal terminado, Iniciando diseño Partida

--- ProyectoIPC22011903872/Controllers/UserController.cs
+++ ProyectoIPC22011903872/Controllers/UserController.cs
@@ -4,6 +4,7 @@ using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 using ProyectoIPC22011903872.Models;
 using ProyectoIPC22011903872.Models.ViewModels;
 
@@ -12,7 +13,12 @@ namespace ProyectoIPC22011903872.Controllers
     public class UserController : Controller
     {
         // GET: User
-        public ActionResult Login() {
+        public ActionResult Login(string Usuario, string Contraseña) {
+            ViewBag.Usuario = Usuario;
+            ViewBag.Contraseña = Contraseña;
+            if(User.Identity.IsAuthenticated){
+                return RedirectToAction("Index", "Principal");
+            }
             return View();
         }
         public ActionResult getUser()
@@ -23,26 +29,43 @@ namespace ProyectoIPC22011903872.Controllers
                 ListaUsuarios = (from d in db.USUARIO
                                  select new getUserViewModel
                                  {
-                                    Nombre = d.Nombre,
-                                    Apellido = d.Apellido,
-                                    Usuario = d.Usuario1,
-                                    Correo_Electronico = d.Correo_Electronico,
-                                    Codigo_Pais = d.Codigo_Pais,
-                                    Fecha_nacimiento = d.Fecha_nacimiento
+                                     Nombre = d.Nombre,
+                                     Apellido = d.Apellido,
+                                     Usuario = d.Usuario1,
+                                     Correo_Electronico = d.Correo_Electronico,
+                                     Codigo_Pais = d.Codigo_Pais,
+                                     Fecha_nacimiento = d.Fecha_nacimiento
                                  }).ToList();
             }
             return View(ListaUsuarios);
         }
-        [HttpGet]
+        [HttpPost]
         public ActionResult Validar(string Usuario, string password) {
-            using (OthelloEntities db = new OthelloEntities()) {
-                if (!String.IsNullOrEmpty(Usuario) && !String.IsNullOrEmpty(password))
+            var us = "";
+            var Contraseña = "";
+            using (OthelloEntities db = new OthelloEntities())
+            {
+                var pass = Encriptar.CrearHASH(password);
+                var usuario2 = db.USUARIO.FirstOrDefault(e => e.Usuario1 == Usuario);
+                var usuario = db.USUARIO.FirstOrDefault(e => e.Usuario1 == Usuario && e.Contraseña == pass);
+                if (usuario2 != null)
                 {
-                    db.USUARIO.FirstOrDefault(e => e.Usuario1 == Usuario && e.Contraseña == Encriptar.CrearHASH(password));
-
+                    if (usuario != null)
+                    {
+                        FormsAuthentication.SetAuthCookie(usuario.Usuario1, true);
+                        return RedirectToAction("Index", "Principal");
+                    }
+                    else
+                    {
+                        Contraseña = "Contraseña Incorrecta";
+                    }
+                }
+                else
+                {
+                    us = "El usuario ingresado no existe.";
                 }
             }
-                return View();
+            return RedirectToAction("Login", "User", new { Usuario = us, Contraseña = Contraseña});
         }
         public ActionResult Registrar() {
             List<getPaisViewModel> ListaPais;
@@ -58,6 +81,10 @@ namespace ProyectoIPC22011903872.Controllers
                              }).ToList();
             }
             ViewBag.paises = ListaPais;
+            if (User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "Principal");
+            }
             return View();
         }
         [HttpPost]
@@ -80,24 +107,39 @@ namespace ProyectoIPC22011903872.Controllers
                     using (OthelloEntities db= new OthelloEntities())
                     {
                         var user = db.USUARIO.FirstOrDefault(e => e.Usuario1 == model.Usuario);
+                        var user2 = db.USUARIO.FirstOrDefault(e => e.Correo_Electronico == model.Correo_Electronico);
                         if (user==null)
                         {
-                            if (model.Password==model.RePassword)
+                            if (user2 == null) { 
+                                if (model.Password==model.RePassword)
+                                {
+                                    var Usuario = new USUARIO();
+                                    Usuario.Nombre = model.Nombre;
+                                    Usuario.Apellido = model.Apellido;
+                                    Usuario.Correo_Electronico = model.Correo_Electronico;
+                                    Usuario.Usuario1 = model.Usuario;
+                                    Usuario.Contraseña = Encriptar.CrearHASH(model.Password);
+                                    Usuario.Codigo_Pais = model.Codigo_Pais;
+                                    Usuario.Fecha_nacimiento = model.Fecha_nacimiento;
+                                    db.USUARIO.Add(Usuario);
+                                    db.SaveChanges();
+                                    return Redirect("~/");
+                                }
+                                else
+                                {
+                                    ViewBag.mensajeContraseña = "Contraseña no coincide";
+                                }
+                            }
+                            else
                             {
-                                var Usuario = new USUARIO();
-                                Usuario.Nombre = model.Nombre;
-                                Usuario.Apellido = model.Apellido;
-                                Usuario.Correo_Electronico = model.Correo_Electronico;
-                                Usuario.Usuario1 = model.Usuario;
-                                Usuario.Contraseña = Encriptar.CrearHASH(model.Password);
-                                Usuario.Codigo_Pais = model.Codigo_Pais;
-                                Usuario.Fecha_nacimiento = model.Fecha_nacimiento;
-                                db.USUARIO.Add(Usuario);
-                                db.SaveChanges();
+                                ViewBag.mensajeCorreo = "Correo ya registrado";
                             }
                         }
+                        else
+                        {
+                            ViewBag.mensajeUsuario = "Usuario ya Registrado";
+                        }
                     }
-                    return Redirect("~/");
                 }
                 return View(model);
             
---
+++ ProyectoIPC22011903872/Funciones.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoIPC22011903872
+{
+    public static class Funciones
+    {
+        public static string IsActive(this HtmlHelper html,string control, string action)
+        {
+            var routeData = html.ViewContext.RouteData;
+            var routeAction = (string)routeData.Values["action"];
+            var routeControl = (string)routeData.Values["controller"];
+            var returnActive = control == routeControl && action == routeAction;
+            return returnActive ? "active" : "joo";
+        }
+    }
+}
